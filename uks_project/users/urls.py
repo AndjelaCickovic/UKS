a344869e283dcf5ef80610dacea711d0494005c9
AndjelaCickovic <51359786+AndjelaCickovic@users.noreply.github.com>
2021-01-30 02:25:14 +0100

    User login implemented

--- uks_project/users/urls.py
+++ uks_project/users/urls.py
@@ -6,5 +6,6 @@ app_name='users'
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
-    url(r'^login/$',views.login,name='login')
+    url(r'^login/$',views.user_login,name='user_login'),
+    
 ]
