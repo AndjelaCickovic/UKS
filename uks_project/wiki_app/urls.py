49bd34164f5f10bb8c15c6ff46e4071d7b7d2346
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-01-30 13:44:48 +0100

    Handled request for page view
    Also enabled links to pages from wiki page

--- uks_project/wiki_app/urls.py
+++ uks_project/wiki_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from wiki_app import views
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.main),
+    path('page/<int:page_id>/', views.page),
 ]
