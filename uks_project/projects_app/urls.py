84d5513cfe3dca037253ff82e7c3b04ef452fb9e
NevenaAl <51334038+NevenaAl@users.noreply.github.com>
2021-01-31 18:18:42 +0100

    Rendered and styled main and new project page

--- uks_project/projects_app/urls.py
+++ uks_project/projects_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from projects_app import views
+from django.urls import path
 
 urlpatterns = [
-    url(r'^$', views.main, name='main')
+    url(r'^$', views.main, name='main'),
+    path('new/', views.new_project),
 ]
