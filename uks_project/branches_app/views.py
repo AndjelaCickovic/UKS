ef4d67a6f75ecf4072facda8f21f28b8859456c9
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-01-31 17:37:12 +0100

    Rendered view for branches

--- uks_project/branches_app/views.py
+++ uks_project/branches_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from branches_app.models import Branch
 
 # Create your views here.
 def main(request):
-    return ('Hello')
+    branches = Branch.objects.order_by('id')
+    obj_dict = {'branches':branches}
+
+    return render(request,'branches_app/main.html',obj_dict)
