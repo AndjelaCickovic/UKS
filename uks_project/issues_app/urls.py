929da3e71c2c89ce2174133a98f3982be8765fe1
Aleksandar Đurđev <58145119+saleee6@users.noreply.github.com>
2021-01-31 14:05:45 +0100

    Create operation for label

--- uks_project/issues_app/urls.py
+++ uks_project/issues_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from issues_app import views
 
 urlpatterns = [
+    url(r'^add-label', views.add_label, name='add_label'),
     url(r'^$', views.main, name='main')
 ]
--- uks_project/issues_app/views.py
+++ uks_project/issues_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from issues_app.models import Issue
+from issues_app.models import Issue, Label
 from issues_app.serializers import IssueSerializer
+from issues_app.forms import LabelForm
 import sys
 import io
 
@@ -11,3 +12,21 @@ def main(request):
     serializer = IssueSerializer(issues, many=True)
     dictionary = {'issues': serializer.data}
     return render(request, 'issues_app/main.html', context=dictionary)
+
+def add_label(request):
+    issues = Issue.objects.all()
+    serializer = IssueSerializer(issues, many=True)
+    form = LabelForm()
+
+    if request.method =='POST' :
+        form = LabelForm(request.POST)
+
+        if form.is_valid(): 
+            label = Label()
+            label.name = form.cleaned_data['name']
+            label.description = form.cleaned_data['description']
+            label.colour = form.cleaned_data['colour']
+            label.save()
+            
+    dictionary = {'issues': serializer.data, 'form': form}
+    return render(request, 'issues_app/label.html', context=dictionary)
