ff85582bf92df939331d362b9996978c86073563
NevenaAl <51334038+NevenaAl@users.noreply.github.com>
2021-02-01 00:12:45 +0100

    Edit project implemented

--- uks_project/projects_app/urls.py
+++ uks_project/projects_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('new/', views.new_project),
     path('close/<int:project_id>/',views.close_project),
     path('reopen/<int:project_id>/',views.reopen_project),
+    path('edit/<int:project_id>/',views.edit_project),
     path('delete/<int:project_id>/',views.delete_project),
 ]
--- uks_project/projects_app/views.py
+++ uks_project/projects_app/views.py
@@ -12,7 +12,6 @@ def main(request):
     return render(request, 'projects_app/main.html', projects_dict)
 
 def new_project(request):
-    projects = Project.objects.all().order_by('id')
     form = ProjectForm()
 
     if request.method =='POST' :
@@ -26,7 +25,28 @@ def new_project(request):
 
             project.save()
 
-            return render(request, 'projects_app/main.html' ,{'projects':projects})
+            return redirect('/projects')
+    
+    return render(request,'projects_app/new_project.html',{'form': form})
+
+def edit_project(request, project_id):
+    try:
+        project = Project.objects.get(id=project_id)
+    except:
+        return redirect('/projects')
+
+    form = ProjectForm(instance=project)
+
+    if request.method =='POST' :
+        form = ProjectForm(request.POST)
+
+        if form.is_valid(): 
+            project.name = form.cleaned_data['name']
+            project.description = form.cleaned_data['description']
+
+            project.save()
+
+            return redirect('/projects')
             
     return render(request,'projects_app/new_project.html',{'form': form})
 
