38716ab1bcaedb97d055e656a22662e827c8e8d5
NevenaAl <51334038+NevenaAl@users.noreply.github.com>
2021-02-01 19:39:46 +0100

    Project details page and column create implemented

--- uks_project/projects_app/urls.py
+++ uks_project/projects_app/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
+    path('<int:project_id>/',views.project),
+    path('<int:project_id>/new_column/',views.new_column),
     path('new/', views.new_project),
     path('close/<int:project_id>/',views.close_project),
     path('reopen/<int:project_id>/',views.reopen_project),
--- uks_project/projects_app/views.py
+++ uks_project/projects_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from projects_app.models import Project, Column
-from projects_app.forms import ProjectForm
+from projects_app.forms import ProjectForm, ColumnForm
 import sys
 import io
 
@@ -29,6 +29,20 @@ def new_project(request):
     
     return render(request,'projects_app/new_project.html',{'form': form})
 
+
+def project(request, project_id):
+    try:
+        project = Project.objects.get(id=project_id)
+    except:
+        return redirect('/projects')
+    
+    objects_dict = {
+        'project':project, 
+        'columns': project.columns.all()
+    }
+    return render(request,"projects_app/project.html",objects_dict)
+
+
 def edit_project(request, project_id):
     try:
         project = Project.objects.get(id=project_id)
@@ -50,6 +64,30 @@ def edit_project(request, project_id):
             
     return render(request,'projects_app/new_project.html',{'form': form})
 
+
+def new_column(request, project_id):
+    try:
+        project = Project.objects.get(id=project_id)
+    except:
+        return redirect('/projects')
+
+    form = ColumnForm()
+
+    if request.method =='POST' :
+        form = ColumnForm(request.POST)
+
+        if form.is_valid(): 
+            column = Column()
+            column.name = form.cleaned_data['name']
+            column.project = project 
+
+            column.save()
+
+            return redirect('/projects/'+ str(project_id))
+    
+    return render(request,'projects_app/new_column.html',{'form': form})
+
+
 def close_project(request,project_id):
     try:
         project = Project.objects.get(id=project_id)
@@ -59,6 +97,7 @@ def close_project(request,project_id):
     project.save()
 
     return redirect('/projects')
+
 
 def reopen_project(request,project_id):
     try:
