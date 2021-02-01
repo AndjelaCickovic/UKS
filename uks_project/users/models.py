c7c1e948ee7123a018dea5bdb373ae3f5f1cb312
AndjelaCickovic <51359786+AndjelaCickovic@users.noreply.github.com>
2021-02-01 15:18:29 +0100

    Created AppUser model

--- uks_project/users/models.py
+++ uks_project/users/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projects_app.models import Project 
 
 # Create your models here.
    
+class AppUser(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    projects = models.ManyToManyField(Project)
+
+    
