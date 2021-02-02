ee378700f9f951374978020d9371895ba55c093d
sarapanic <sarapanic97@gmail.com>
2021-02-02 23:30:20 +0100

    Repository model added

--- uks_project/repositories_app/models.py
+++ uks_project/repositories_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField(max_length = 264)
+    description = models.CharField(max_length = 264, blank = True)
+    is_public = models.BooleanField()
