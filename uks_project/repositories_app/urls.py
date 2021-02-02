257b9130175833815c8e396fa4c5ec0f798d961d
sarapanic <sarapanic97@gmail.com>
2021-02-02 23:55:50 +0100

    Page for all repositories added

--- uks_project/repositories_app/urls.py
+++ uks_project/repositories_app/urls.py
@@ -3,5 +3,6 @@ from repositories_app import views
 from django.urls import path
 
 urlpatterns = [
-    url(r'^$', views.main, name = 'main')
+    path('repository/<int:repository_id>', views.repository, name= 'view_repository'),
+    url(r'^$', views.main, name = 'main'),
 ]
--- uks_project/repositories_app/views.py
+++ uks_project/repositories_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from repositories_app.models import Repository
 from django.urls import reverse
 
 # Create your views here.
 def main(request):
-    return render(request, 'repositories_app/repositories.html')
+    repositories = Repository.objects.all()
+    dictionary = {'repositories': repositories}
+    return render(request, 'repositories_app/repositories.html', context = dictionary)
+
+def repository(request, repository_id):
+    repository = Repository.objects.get(id = repository_id)
+    dictionary = { 'repository': repository}
+    return render(request, 'repositories_app/repository.html', context = dictionary)
