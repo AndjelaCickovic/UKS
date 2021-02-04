2886a79b7621835450e3b5802c4e4726246f4f5e
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-02-04 22:44:09 +0100

    Deleted / at the end of the path

--- uks_project/wiki_app/urls.py
+++ uks_project/wiki_app/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.main),
-    path('/error/', views.error),
-    path('/page/<int:page_id>/', views.page),
+    path('/error', views.error),
+    path('/page/<int:page_id>', views.page),
     path('/page/<int:page_id>/edit', views.edit_page),
     path('/page/<int:page_id>/delete', views.delete_page),
-    path('/page/new/', views.new_page),
+    path('/page/new', views.new_page),
 ]
