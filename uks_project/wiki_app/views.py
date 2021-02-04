b11161d46d4e040e8405fbbf4881e077f3edcea6
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-02-04 22:45:08 +0100

    Changed the way pages are fetched from db

--- uks_project/wiki_app/views.py
+++ uks_project/wiki_app/views.py
@@ -3,44 +3,39 @@ from django.http import HttpResponse
 from wiki_app.models import Wiki, Page
 from wiki_app.forms import PageForm
 from repositories_app.models import Repository
-from repositories_app.serializers import RepositorySerializer
 
 # Create your views here.
 
 def main(request, repository_id):
-    pages = Page.objects.order_by('id')
     repository = Repository.objects.get(id=repository_id)
-    repo = RepositorySerializer(repository)
+    pages = repository.wiki.all()[0].pages.all()
 
-    obj_dict = {'pages':pages, 'repository': repo.data}
+    obj_dict = {'pages':pages, 'repository': repository}
 
     return render(request,'wiki_app/no_page.html',obj_dict)
 
 def error(request, repository_id):
-    pages = Page.objects.order_by('id')
     repository = Repository.objects.get(id=repository_id)
-    repo = RepositorySerializer(repository)
+    pages = repository.wiki.all()[0].pages.all()
 
-    obj_dict = {'pages':pages, 'repository': repo.data}
+    obj_dict = {'pages':pages, 'repository': repository}
 
     return render(request,'wiki_app/error.html',obj_dict)
 
 def page(request,page_id, repository_id):
-    pages = Page.objects.order_by('id')
     try:
         page = Page.objects.get(id=page_id)
     except:
         return redirect('/repositories/repository/' + str(repository_id) + '/wiki/error')
     
     repository = Repository.objects.get(id=repository_id)
-    repo = RepositorySerializer(repository)
+    pages = repository.wiki.all()[0].pages.all()
 
-    obj_dict = {'page':page, 'pages':pages, 'repository': repo.data}
+    obj_dict = {'page':page, 'pages':pages, 'repository': repository}
 
     return render(request,'wiki_app/page.html',obj_dict)
 
 def new_page(request, repository_id):
-    pages = Page.objects.order_by('id')
     form = PageForm()
 
     if request.method =='POST' :
@@ -58,12 +53,11 @@ def new_page(request, repository_id):
             return redirect('/repositories/repository/' + str(repository_id) + '/wiki/page/' + str(page.id))
 
     repository = Repository.objects.get(id=repository_id)
-    repo = RepositorySerializer(repository)
+    pages = repository.wiki.all()[0].pages.all()
 
-    return render(request,'wiki_app/new_page.html',{'form':form, 'pages': pages, 'repository': repo.data})
+    return render(request,'wiki_app/new_page.html',{'form':form, 'pages': pages, 'repository': repository})
 
 def edit_page(request, page_id, repository_id):
-    pages = Page.objects.order_by('id')
     try:
         page = Page.objects.get(id=page_id)
     except:
@@ -82,19 +76,18 @@ def edit_page(request, page_id, repository_id):
             return redirect('/repositories/repository/' + str(repository_id) + '/wiki/page/' + str(page.id))
 
     repository = Repository.objects.get(id=repository_id)
-    repo = RepositorySerializer(repository)
+    pages = repository.wiki.all()[0].pages.all()
 
-    return render(request,'wiki_app/new_page.html',{'form':form, 'pages': pages, 'repository': repo.data})
+    return render(request,'wiki_app/new_page.html',{'form':form, 'pages': pages, 'repository': repository})
 
 def delete_page(request, page_id, repository_id):
-    pages = Page.objects.order_by('id')
     try:
         page = Page.objects.get(id=page_id)
     except:
         repository = Repository.objects.get(id=repository_id)
-        repo = RepositorySerializer(repository)
+        pages = repository.wiki.all()[0].pages.all()
 
-        return render(request, 'wiki_app/error.html' ,{'pages':pages, 'repository': repo.data})
+        return render(request, 'wiki_app/error.html' ,{'pages':pages, 'repository': repository})
 
     page.delete()
 
