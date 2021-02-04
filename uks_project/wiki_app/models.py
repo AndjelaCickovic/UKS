58f46e70d7830944624640ac686f3605834add90
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-02-04 22:44:45 +0100

    Changed wiki and page model

--- uks_project/wiki_app/models.py
+++ uks_project/wiki_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from repositories.models import Repository
+from repositories_app.models import Repository
 
 class Wiki(models.Model):
-    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    repository = models.ForeignKey(Repository, related_name='wiki', on_delete=models.CASCADE)
 
 class Page(models.Model):
     title = models.CharField(max_length=264)
     content = models.TextField()
     message = models.CharField(max_length=264, blank=True)
-    wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE)
+    wiki = models.ForeignKey(Wiki, related_name='pages', on_delete=models.CASCADE)
