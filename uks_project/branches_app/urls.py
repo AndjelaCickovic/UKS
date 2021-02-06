c4ada1f437b0c3807a330515bf71fb51073bf048
AndjelaCickovic <51359786+AndjelaCickovic@users.noreply.github.com>
2021-02-06 23:07:51 +0100

    Create operation for commits implemented

--- uks_project/branches_app/urls.py
+++ uks_project/branches_app/urls.py
@@ -5,9 +5,17 @@ from django.urls import path
 app_name='branches_app'
 
 urlpatterns = [
-    url(r'^$', views.main,name='branches_view'),
     url(r'new',views.new_branch,name='new_branch'),
     url(r'edit/(?P<branch_id>\d+)',views.edit_branch,name='edit_branch'),
     url(r'delete/(?P<branch_id>\d+)',views.delete_branch,name='delete_branch'),
-    url(r'(?P<branch_id>\d+)',views.commits,name='commits')
+    url(r'add-commit/(?P<branch_id>\d+)',views.new_commit,name='new_commit'),
+    url(r'(?P<branch_id>\d+)/commits',views.commits,name='commits'),
+    url(r'^$', views.main,name='branches_view')
+
+    # path('new/',views.new_branch),
+    # path('edit/<int:branch_id>/',views.edit_branch),
+    # path('delete/<int:branch_id>/',views.delete_branch),
+    # path('<int:branch_id>/commit/new/',views.new_commit),
+    # path('<int:branch_id>/commits',views.commits),
+    # path('^$', views.main)
 ]
