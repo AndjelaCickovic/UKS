b1561af269ae567ad8fa7f40ecc4ac56c9d6477b
AndjelaCickovic <51359786+AndjelaCickovic@users.noreply.github.com>
2021-02-15 14:25:18 +0100

    Written tests for user profile edit

--- uks_project/users/views.py
+++ uks_project/users/views.py
@@ -21,7 +21,7 @@ def register(request):
         user_form = UserForm(request.POST)
 
         if user_form.is_valid():
-
+            print('USER')
             user = user_form.save()
             user.set_password(user.password)
             user.save()
