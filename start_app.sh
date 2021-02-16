15debbb8e292faa6fd7d0b47503655e1fe8eeb6b
sarapanic <sarapanic97@gmail.com>
2021-02-16 20:23:04 +0100

    Added run tests in start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -2,4 +2,5 @@
 
 python uks_project/manage.py makemigrations
 python uks_project/manage.py migrate
+python uks_project/manage.py test users issues_app projects_app wiki_app
 python uks_project/manage.py runserver 0.0.0.0:8000
